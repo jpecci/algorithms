a75752e4521f7cac9dc3af6abf07a87c66f97566
Jacopo <jacopo.pecci@gmail.com>
2014-08-08 17:07:24 +0100

    cleaned code in hashtable.py

--- util/hashtable.py
+++ util/hashtable.py
@@ -10,20 +10,23 @@ class Entry:
 
 class HashTable:
 	def __init__(self, capacity=16):
-		self.v=[None]*capacity
+		self.buckets=[None]*capacity
 		self.capacity=capacity
 		self.size=0
 		self.loadfactor=0.5
-	def findPos(self, hashcode, capacity):
-		return abs(hashcode)%capacity
+
+	def __hash2pos(self, hashcode):
+		return abs(hashcode)%self.capacity
+	
 	def add(self, key, value):
-		pos=self.findPos(hash(key),self.capacity)
-		if self.v[pos] is None:
-			self.v[pos]=Entry(key,value,None)
+		pos=self.__hash2pos(hash(key))
+		if self.buckets[pos] is None:
+			self.buckets[pos]=Entry(key,value,None)
 			self.size+=1
 			return None
 
-		entry=self.v[pos]
+		#scroll throught the bucket to update
+		entry=self.buckets[pos]
 		while entry is not None:
 			if entry.key == key:
 				old_value=entry.value
@@ -31,61 +34,64 @@ class HashTable:
 				return old_value
 			entry=entry.next
 
-		first_entry=self.v[pos]
+		#append at the front
+		first_entry=self.buckets[pos]
 		new_entry=Entry(key,value, first_entry)
-		self.v[pos]=new_entry
+		self.buckets[pos]=new_entry
 		self.size+=1
-		self.resize()
+		self.__resize()
 		return None		
 		
-	def get(self, key):
-		pos=self.findPos(hash(key),self.capacity)
-		if self.v[pos] is None:
+	def get_value(self, key):
+		pos=self.__has2pos(hash(key))
+		if self.buckets[pos] is None:
 			return None
-		entry = self.v[pos]
+
+		#scroll throught the bucket
+		entry = self.buckets[pos]
 		while entry is not None:
 			if entry.key==key:
 				return entry.value		
 			entry=entry.next
 		return None
 
-	def remove(self, key):
-		pos=self.findPos(hash(key),self.capacity)
+	def remove_entry(self, key):
+		pos=self.__has2pos(hash(key))
 		remove=False
-		entry=self.v[pos]
-		prev_entry=entry
-		while entry is not None:
-			if entry.key==key:
+		cursor=self.buckets[pos]
+		prev_cur=cursor
+		while cursor is not None:
+			if cursor.key==key:
 				remove=True
 				break
-			prev_entry=entry
-			entry=entry.next
+			prev_cur=cursor
+			cursor=cursor.next
 		if remove:
 			self.size-=1
-			if prev_entry is self.v[pos]:
-				self.v[pos]=entry.next
+			if prev_cur is self.v[pos]:
+				self.buckets[pos]=cursor.next
 			else:
-				prev_entry.next=entry.next
-			return entry
+				prev_cur.next=cursor.next
+			return cursor
 		return None
 
-	def resize(self):
+	def __resize(self):
 		if self.size > self.loadfactor*self.capacity:
-			newCapacity=2*self.capacity
-			print "resizing %d->%d..."%(self.capacity,newCapacity)
-			self.capacity=newCapacity
+			self.capacity*=2
+			#print "resizing %d->%d..."%(self.capacity,newCapacity)
 			self.size=0
-			temp=self.v
-			self.v=[None]*newCapacity
-			for row in temp:
-				entry=row
+			temp=self.buckets
+			self.buckets=[None]*self.capacity
+			for bucket in temp:
+				entry=bucket
 				while entry is not None:
 					self.add(entry.key, entry.value)
 					entry=entry.next
 
 	def __str__(self):
 		output=""
-		for entry in self.v:
+		for bucket in self.buckets:
+			entry=bucket
 			if entry is None:
 				output+="None\n"
 			else:
@@ -94,6 +100,8 @@ class HashTable:
 					entry=entry.next
 				output+="\n"
 		return output
+	def __repr__(self):
+		return self.__str__()
 
 if __name__=='__main__':
 	h=HashTable(capacity=3)
