e0f840fed5393dbac2eb0ee26cac7d06673cd2ed
Jacopo <jacopo.pecci@gmail.com>
2014-08-11 18:24:46 +0100

    cleaned code for trie.py

--- tree/trie.py
+++ tree/trie.py
@@ -1,86 +1,102 @@
 
+NUM_ASCII_CHARS=256
+
 class Node:
-	def __init__(self, letter, value, num_children=256):
+	def __init__(self, letter, value):
 		self.parent=None
-		self.letter=letter
+		self.letter=letter #this is reduntant, just handy
 		self.value=value
-		self.children=[None]*num_children
+		self.children=[None]*NUM_ASCII_CHARS
 	def __str__(self):
 		num_children=len([c for c in self.children if c is not None])
 		return "%s,%d,%d"%(self.letter,self.value,num_children)
 	def __repr__(self):
 		return self.__str__()
-#----------
-# REQUIRES SOME TESTING
-#----------
+ 
 class TrieTree:
 	def __init__(self):
 		self.root=Node("",0)
-		self.size=0
-	def add_word(self, word, tree):
+		self.num_words=0
+
+	def add_word(self, word):
+		self.__add_word(word, self.root)
+	
+	def __add_word(self, word, tree):
 		if len(word)<1:
 			return
 		letter=word[0]
 		idx=ord(letter)
+		#create the node if it doesnt exist
 		if tree.children[idx] is None:
 			tree.children[idx]=Node(letter,0)
+
+		child=tree.children[idx]
 		if len(word)==1:
-			if tree.children[idx].value==0:
-				self.size+=1 #increment only for new words
-			tree.children[idx].value+=1 #frequency
+			if child.value==0:
+				self.num_words+=1 #increment only if new word
+			child.value+=1 #frequency
 		else:
-			self.add_word(word[1:],tree.children[idx])
+			self.__add_word(word[1:], child)
 		
 	def words_starting(self, prefix):
-		cur=self.__find(prefix, self.root)
-		if cur is None:
+		'''
+		return a list of all the words with the prefix
+		'''
+		cursor=self.__find(prefix, self.root)
+		if cursor is None:
 			return []
-		return self.__get_words(cur,prefix)
-	def __get_words(self, tree, pre_word):
+
 		words=[]
-		def helper(tree, pre_word):
+		def helper(pre_word, tree):
 			for child in tree.children:
 				if child is not None:
 					word=pre_word+child.letter
 					if child.value>0:
 						words.append(word)
-					helper(child, word)
-		helper(tree,pre_word)
+					helper(word, child)
+
+		helper(prefix, cursor)
 		return words
+
+
 	def __find(self, word, tree):
+		'''return None if the word is not found'''
 		if len(word)==0:
 			return tree
 
 		idx=ord(word[0])
+		child=tree.children[idx]
 		if len(word)==1:
-			return tree.children[idx]
+			return child
 		else:
-			if tree.children[idx] is None:
+			if child is None:
 				return None
-			return self.__find(word[1:], tree.children[idx])
+			return self.__find(word[1:], child)
 	
 	def get_value(self, word):
 		node= self.__find(word, self.root)
 		if node is None:
-			return  0
+			return  -1
 		return node.value  
 
 	def contains(self, word):
 		node=self.__find(word, self.root)
 		if node is None:
 			return False
-		return node.value>0
+		return node.value>0 #0 means this is only a prefix
 
 	def remove(self, word):
 		node=self.__find(word, self.root)
 		if node is not None:
-			self.size-=node.value
+			if node.value >0: #decrement only if it was a word 
+				self.num_words-=1
 			node.value=0
+
 
 if __name__=='__main__':
 	t=TrieTree()
 	fp=open('/Users/jacopo/Downloads/alice_in_wonderland.txt','r')
-	prune=["'",', '.', ';', '?', 's']
+	prune=["'", ',', '.', ';', '?', '!' ,':', 's']
 	count_words=0
 	for line in fp:
 		words=line.strip().split(" ")
@@ -89,7 +105,7 @@ if __name__=='__main__':
 				if word.endswith(x):
 					word=word[:-1]
 			count_words+=1
-			t.add_word(word, t.root)
+			t.add_word(word)
 	fp.close()
 	words=t.words_starting("")
 	swords=sorted(words,  key=lambda w:t.get_value(w))
