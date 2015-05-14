ff84b03d1af0b81709a0204d5b609a071f015381
Jacopo <jacopo.pecci@gmail.com>
2015-05-14 17:22:13 +0100

    new file:   generators.py

---
+++ generators.py
@@ -0,0 +1,24 @@
+import itertools as it
+
+def file_reader(filename, separator=','):
+	with open(filename) as fp:
+		for line in fp:
+			tks=[tk.strip() for tk in line.split(separator)]
+			yield (int(tks[0]), float(tks[1]))
+
+get_time=lambda date: date%10000
+get_min =lambda date: get_time(date)%100
+get_hour =lambda date: get_time(date)//100
+
+get_date=lambda date: date//10000
+get_day=lambda date: get_date(date)%100
+get_month=lambda date: (get_date(date)%10000)//100
+
+
+
+
+if __name__=='__main__':
+	fn='/Users/jacopo/Downloads/dummy.csv'
+	reader=file_reader(fn)
+	for hour, iterator in it.groupby(reader, lambda item:get_month(item[0])):
+		print "hour={}, len={}".format(hour, len(list(iterator)))
