d53457929087176c8a444262aba13a302d5b7619
Jacopo <jacopo.pecci@gmail.com>
2015-05-21 18:56:03 +0100

    new file:   decorators.py

---
+++ decorators.py
@@ -0,0 +1,31 @@
+import time
+
+def time_func(f):
+	"""time the running time of a function"""
+	def helper(*args, **kargs):
+		start=time.time()
+		ans=f(*args, **kargs)
+		end=time.time()
+		dt=end-start
+		if dt<60:
+			msg="{0} took {1:.3f} secs".format(f.__name__, dt)
+		else:
+			msg="{0} took {1:.3f} mins".format(f.__name__, dt/60.)
+		print msg
+		return ans
+	return helper
+
+def mark_call(f):
+	"""print an output any time the function is called"""
+	def helper(*args, **kargs):
+		ans=f(*args, **kargs)
+		print "called {}".format(f.__name__)
+		return ans
+	return helper
+
+@mark_call
+def dummy(x,y):
+	return x+y
+
+if __name__=='__main__':
+ 	print dummy(1, y=2)
