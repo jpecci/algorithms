f8b6507ed9e18c47a944da6d5d039d9213cb1b92
Jacopo <jacopo.pecci@gmail.com>
2015-05-23 18:22:25 +0100

    renamed file and added few functions

--- listfuncs.py
+++ listfuncs.py
@@ -1,5 +1,21 @@
+import itertools as 
+
+def flatten(xss):
+	"""flatten a list of a list"""
+	return [x for xs in xss for x in xs]
+def iflatten(xss):
+	for xs in xss:
+		for x in xs:
+			yield x
+
+def flatMap(xs, op):
+	return flatten(map(op, xs))
+def iflatMap(xs, op):
+	return iflatten(it.imap(op,xs))
+
 def foldRight_(xs, zero, op):
-	#op(x0, op(x1, op(x2,zero)))
+	"""op(x0, op(x1, op(x2,zero)))
+	recursive implementation"""
 	def helper(ys, acc):
 		if len(ys)==0:
 			return acc
@@ -9,14 +25,15 @@ def foldRight_(xs, zero, op):
 	return helper(xs, zero)
 
 def foldRight(xs, zero, op):
-	#op(x0, op(x1, op(x2,zero)))
+	"""op(x0, op(x1, op(x2,zero)))"""
 	y=zero
 	for x in xs[::-1]:
 		y=op(x,y)
 	return y
 
 def foldLeft_(xs, zero, op):
-	#op(op(op(zero,x0), x1), x2)
+	"""op(op(op(zero,x0), x1), x2)
+	recursive implementation"""
 	def helper(ys, acc):
 		if len(ys)==0:
 			return acc
@@ -26,7 +43,7 @@ def foldLeft_(xs, zero, op):
 	return helper(xs, zero)
 
 def foldLeft(xs, zero, op):
-	#op(op(op(zero,x0), x1), x2)
+	"""op(op(op(zero,x0), x1), x2)"""
 	y=zero
 	for x in xs:
 		y=op(y,x)
@@ -39,3 +56,4 @@ if __name__=="__main__":
 	print foldRight_(xs, 1, operator.div)
 	print foldLeft(xs, 1, operator.div)
 	print foldLeft_(xs, 1, operator.div)
+	print flatten([[1,2,3],[4,5]])
