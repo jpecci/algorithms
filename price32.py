958902b378edfb4c7f73be49632029e4add7c2c0
Jacopo <jacopo.pecci@gmail.com>
2015-05-24 17:20:42 +0100

    chaged as subclass of Fraction

--- price32.py
+++ price32.py
@@ -1,80 +1,38 @@
 from fractions import Fraction
 
 
-class Price32:
-	def __init__(self, value, *args):
+class Price32(Fraction):
+	def __new__(cls, *args):
+		return Fraction.__new__(cls, args)
+	def __init__(self, *args):
 		"""
 		accepted constructors:
-		Price32(10), Price32(10,1,32), Price32(10, Fraction(1,32))
+		Price32(10), Price32(10,1,32)
 		"""
-		self.value=value
-
-		if len(args)==0:
-			self.frac=Fraction(0)
-		elif len(args)==1 and isinstance(args[0], Fraction):
-			frac=args[0]
-			self.frac=frac
-		elif len(args)==2 and isinstance(args[0], int) and isinstance(args[1], int):
-			num, den=args
-			self.frac=Fraction(num, den)
+		
+		if len(args)==1:
+			Fraction.__new__(args[0])
+		elif len(args)==3:
+			value, num, den=args
+			Fraction.__new__(value*den+num, den)
  		else:
  			raise Exception("wrong arguments")
 
- 		self._simplify()
+ 	def _express_in_32(self):
+ 		value=self.numerator//self.denominator
+ 		num=self.numerator%self.denominator
+		den=self.denominator
+		return value, num, den
+		
 
- 	def _simplify(self):
- 		f=self.frac
- 		if f>0 and f.numerator>=f.denominator:
- 			self.value+= int(f.numerator/f.denominator)
-			self.frac= Fraction(f.numerator%f.denominator, f.denominator)
- 	 	elif f<0:
- 	 		raise NotImplemented
-
-
-	def __add__(self, other):
-		if isinstance(other, Price32):
-			return Price32(self.value+other.value, self.frac+other.frac)
-		elif isinstance(other, int):
-			return Price32(self.value+other, self.frac)
-		elif isinstance(other, Fraction):
-			#TODO 5 1/32 - 1/16 does not work
-			return Price32(self.value, self.frac+other)
-		else:
-			raise Exception("can only add int or Price32")
-		 
-	def __neg__(self):
-		return Price32(-self.value, -self.frac)
-
-	def __sub__(self, other):
-		return self+(-other)
-
-
-	def __mul__(self, other):
-		if isinstance(other, Price32):
-			return self.toFloat()*other.toFloat()
-		elif isinstance(other, int):
-			return Price32(self.value*other, self.frac*other)
-		elif isinstance(other, float):
-			return self.toFloat()*other
-
-		"""
-		if isinstance(other, Price32):
-			value=self.value*other.value
-			frac=self.value*other.frac + self.frac*other.value + self.frac*other.frac
-			return Price32(value, frac)
-		"""
-
-	def __div__(self, other):
-		if isinstance(other, Price32):
-			return self.toFloat()/other.toFloat()
-		return self.toFloat()*(1.0/other)
 
 	def __str__(self):
-		return "{:d} {:d}/{:d}".format(self.value, abs(self.frac.numerator), self.frac.denominator)
+		value, num, den = self._express_in_32()
+		return "{:d} {:d}/{:d}".format(value, num, den)
 	def __repr__(self):
 		return str(self)
 	def toFloat(self):
-		return self.value+float(self.frac.numerator)/self.frac.denominator
+		return float(self.numerator)/self.denominator
 
 
 
