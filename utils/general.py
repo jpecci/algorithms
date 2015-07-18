413e1e285576afcaaaddbfda8ba79a68be305e71
Jacopo <jacopo.pecci@gmail.com>
2015-07-18 11:06:39 +0100

    deleted:    utils/general.py

--- utils/general.py
+++
@@ -1,9 +0,0 @@
-import time
-
-def time_func(f,*args):
-	print "%s running..."%f.__name__
-	start=time.time()
-	res=f(*args)
-	end=time.time()
-	print "\tfinished in %.4f mins"%( (end-start)/60.)
-	return res
